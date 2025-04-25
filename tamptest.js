258787507b8b01dd0d76188249ffa268910a3c7f
iartan <80929458+iartan@users.noreply.github.com>
2025-04-25 23:04:41 +0200

    Create tamptest.js

---
+++ tamptest.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Green Everything
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-25
+// @description  Turn everything green!
+// @author       You
+// @match        https://www.google.ch/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.ch
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.body.style.backgroundColor = 'green';
+    document.body.style.color = 'green';
+})();
