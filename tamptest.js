3319c69e13807f40851ec185fa48c1d64f21ae29
iartan <80929458+iartan@users.noreply.github.com>
2025-04-25 23:35:10 +0200

    Update tamptest.js

--- tamptest.js
+++ tamptest.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Green Everything
 // @namespace    http://tampermonkey.net/
-// @version      2025-04-25
+// @version      202504251
 // @description  Turn everything green!
 // @author       You
 // @match        https://www.google.ch/
